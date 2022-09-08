19e00754b918aedf4decfdb106d2c362e0c30139
Motdep@sse <lirsitogo2021@gmail.com>
2022-09-08 16:33:49 +0000

    modified:   views/index.html

--- views/js/main.js
+++ views/js/main.js
@@ -57,8 +57,135 @@ window.onload = function () {
     });
 
 
+    // Chart dans le right bar
+    
+var chartDashed = new CanvasJS.Chart("chartContainerDashed", {
+	animationEnabled: true,
+	title: {
+		text: "Etat récapitulatif de la semaine"
+	},
+	axisX: {
+		valueFormatString: "DDD",
+		minimum: new Date(2017, 1, 5, 23),
+		maximum: new Date(2017, 1, 12, 1)
+	},
+/* 	axisY: {
+		title: "Number of Messages"
+	}, */
+	legend: {
+		verticalAlign: "top",
+		horizontalAlign: "right",
+		dockInsidePlotArea: true
+	},
+	toolTip: {
+		shared: true
+	},
+	data: [{
+		name: "Lomé",
+		showInLegend: true,
+		legendMarkerType: "square",
+		type: "area",
+		color: "rgba(40,175,101,0.6)",
+		markerSize: 0,
+		dataPoints: [
+			{ x: new Date(2017, 1, 6), y: 30 },
+			{ x: new Date(2017, 1, 7), y: 40 },
+			{ x: new Date(2017, 1, 8), y: 20 },
+			{ x: new Date(2017, 1, 9), y: 12 },
+			{ x: new Date(2017, 1, 10), y: 50 },
+			{ x: new Date(2017, 1, 11), y: 60 },
+			{ x: new Date(2017, 1, 12), y: 15 }
+		]
+	},
+	{
+		name: "Maritime",
+		showInLegend: true,
+		legendMarkerType: "square",
+		type: "area",
+		color: "rgba(0,75,141,0.7)",
+		markerSize: 0,
+		dataPoints: [
+			{ x: new Date(2017, 1, 6), y: 42 },
+			{ x: new Date(2017, 1, 7), y: 34 },
+			{ x: new Date(2017, 1, 8), y: 29 },
+			{ x: new Date(2017, 1, 9), y: 42 },
+			{ x: new Date(2017, 1, 10), y: 53},
+			{ x: new Date(2017, 1, 11), y: 15 },
+			{ x: new Date(2017, 1, 12), y: 12 }
+		]
+	},
+
+]
+});
+
+
+   
+var chartDashedMoi = new CanvasJS.Chart("chartContainerDashedMoi", {
+	animationEnabled: true,
+	title: {
+		text: "Etat récapitulatif du mois"
+	},
+	axisX: {
+		valueFormatString: "DDD",
+		minimum: new Date(2017, 1, 5, 23),
+		maximum: new Date(2017, 1, 12, 1)
+	},
+/* 	axisY: {
+		title: "Number of Messages"
+	}, */
+	legend: {
+		verticalAlign: "top",
+		horizontalAlign: "right",
+		dockInsidePlotArea: true
+	},
+	toolTip: {
+		shared: true
+	},
+	data: [{
+		name: "Lomé",
+		showInLegend: true,
+		legendMarkerType: "square",
+		type: "area",
+		color: "rgba(40,175,101,0.6)",
+		markerSize: 0,
+		dataPoints: [
+			{ x: new Date(2017, 1, 6), y: 20 },
+			{ x: new Date(2017, 1, 7), y: 15 },
+			{ x: new Date(2017, 1, 8), y: 10 },
+			{ x: new Date(2017, 1, 9), y: 8 },
+			{ x: new Date(2017, 1, 10), y: 33 },
+			{ x: new Date(2017, 1, 11), y: 12 },
+			{ x: new Date(2017, 1, 12), y: 15.5 }
+		]
+	},
+	{
+		name: "Maritime",
+		showInLegend: true,
+		legendMarkerType: "square",
+		type: "area",
+		color: "rgba(0,75,141,0.7)",
+		markerSize: 0,
+		dataPoints: [
+			{ x: new Date(2017, 1, 6), y: 21.1 },
+			{ x: new Date(2017, 1, 7), y: 22.9 },
+			{ x: new Date(2017, 1, 8), y: 22.6 },
+			{ x: new Date(2017, 1, 9), y: 30 },
+			{ x: new Date(2017, 1, 10), y: 10},
+			{ x: new Date(2017, 1, 11), y: 15 },
+			{ x: new Date(2017, 1, 12), y: 12 }
+		]
+	},
+
+]
+});
+    
+    
+    
+    
     chart.render();
     chart1.render();
+    chartDashed.render();
+    chartDashedMoi.render();
 
 
     }
