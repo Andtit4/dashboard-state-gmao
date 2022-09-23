3b454cb3393db9344bebdf8a39cc746792b2f246
Motdep@sse <lirsitogo2021@gmail.com>
2022-09-23 17:54:02 +0000

    new file:   dashboard.php

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
 <?php 
+session_start();
 require('./config/db/db.php');
 
 if (isset($_POST['connexion'])){
     if (!empty($_POST['username'] && $_POST['password'])){
         $username = htmlspecialchars($_POST['username']);
         $password = htmlspecialchars($_POST['password']); 
+        $req = $bdd -> query("SELECT * FROM admins WHERE email = '$username' AND pass_admin = '$password'");
+        $nb = $req -> rowCount();
+        if ($nb > 0){
+            $userInfo = $req -> fetch();
+            $_SESSION['id_admin'] = $userInfo['id_admin'];
+            header("Location: dashboard.php?id_admin=".$_SESSION['id_admin']);
+        }
+        else{
+            $message = "Username ou mot de passe incorrect!";
+        }
+
+        // Checking if it supervisor
+        $req1 = $bdd -> query("SELECT * FROM superviseur WHERE email_sup = '$username' AND pass_sup = '$password'");
+        $nb1 = $req1 -> rowCount();
+        if ($nb1 > 0){
+            $userInfo = $req1 -> fetch();
+            $_SESSION['id_sup'] = $userInfo['id_sup'];
+            header("Location: stateboard.php?id_sup=".$_SESSION['id_sup']);
+        }
+        else{
+            $message = "Username ou mot de passe incorrect!";
+        }
     }
     else{
-        $message = "Username ou mot de passe incorrect!";
+        $message = "Veuillez remplir tous les champs!";
     }
 }
 // echo $now;
--- stateboard.php
+++ stateboard.php
@@ -2,10 +2,10 @@
 require('./config/db/db.php');
 
 $req = $bdd -> query("SELECT * FROM demande ORDER BY date_envoi ASC");
-
+/* 
 $now = date("H:i:s");
 echo $now;
-
+ */
 include('views/index.html');
 
 ?>
