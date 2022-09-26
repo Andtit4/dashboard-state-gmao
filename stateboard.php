20722ee9dda6e141e859282c5aa3b16525f78d91
Motdep@sse <lirsitogo2021@gmail.com>
2022-09-26 16:05:08 +0000

    modified:   stateboard.php

--- stateboard.php
+++ stateboard.php
@@ -1,7 +1,7 @@
 <?php 
 require('config/db/db.php');
 
-$req = $bdd -> query("SELECT * FROM demande ORDER BY date_envoi ASC");
+$req = $bdd -> query("SELECT * FROM demande WHERE type_demande = 'HS' ORDER BY date_envoi ASC");
 
 include('views/stateboard.php');
 
