e9d985a0e7fcf53f89756758f5e8640a3dfe5866
Motdep@sse <lirsitogo2021@gmail.com>
2022-09-29 15:45:39 +0000

    prototype temps écoulé

--- stateboard.php
+++ stateboard.php
@@ -5,8 +5,8 @@ $req = $bdd -> query("SELECT * FROM demande WHERE type_demande = 'HS' ORDER BY d
 
 // Comptage de tous les demandes de site hors service
 $req0 = $bdd -> query("SELECT * FROM demande WHERE type_demande = 'HS' ");
-$req_count_all_hs = $req0 -> rowCount();
-echo $req_count_all_hs;
+$nb_site_all_hs = $req0 -> rowCount();
+echo $nb_site_all_hs;
 
 // Joincture avec la table des sla
 $req1 = $bdd -> query("SELECT *  FROM demande , site WHERE demande.type_demande = 'HS' AND demande.site_demande = site.nom ");
@@ -14,7 +14,15 @@ $nb_site_hs = $req1->rowCount();
 
 $i = $req1->fetch();
 $sla = intval($i['sla']);
-echo $i['sla'];
+echo $i['date_envoi'];
+$date = htmlspecialchars($i['date_envoi']);
+$date1 = new DateTime($date);
+// Difference entre les dates
+$now = new DateTime();
+
+// $diff = date_diff($now, $i['date_envoi']);
+
+echo "\n".$date1->diff($now)->format("%d day %h hour M %i minute");
 
 include('views/stateboard.php');
 
