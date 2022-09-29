9d2ab931fbd86b12895fe6bffcd8652e698d9c7a
Motdep@sse <lirsitogo2021@gmail.com>
2022-09-29 15:21:53 +0000

    recuperation du sla

--- stateboard.php
+++ stateboard.php
@@ -10,7 +10,11 @@ echo $req_count_all_hs;
 
 // Joincture avec la table des sla
 $req1 = $bdd -> query("SELECT *  FROM demande , site WHERE demande.type_demande = 'HS' AND demande.site_demande = site.nom ");
+$nb_site_hs = $req1->rowCount();
 
+$i = $req1->fetch();
+$sla = intval($i['sla']);
+echo $i['sla'];
 
 include('views/stateboard.php');
 
