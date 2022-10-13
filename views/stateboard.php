ee771854c0160864e2ec6a271164abae0cc8d469
Motdep@sse <lirsitogo2021@gmail.com>
2022-10-13 11:14:21 +0000

    update

--- views/stateboard.php
+++ views/stateboard.php
@@ -26,120 +26,117 @@
       <div id="datas">
         <?php  //echo json_encode($data_entry_day_circle, JSON_NUMERIC_CHECK); 
         ?>
-        <script defer>
-          window.onload = function() {
-            // datas = document.getElementById("datas");
-            console.log("datas loaded" );
-            var chart1 = new CanvasJS.Chart("chartContainer", {
-              animationEnabled: true,
-              exportEnabled: true,
-              title: {
-                text: "Rapport journalier global"
-              },
-              subtitles: [{
-                text: "Toutes zones incluses"
-              }],
-              data: [{
-                type: "pie",
-                showInLegend: "true",
-                legendText: "{label}",
-                indexLabelFontSize: 16,
-                indexLabel: "{label} - #percent%",
-                yValueFormatString: "฿#,##0",
-                dataPoints: <?php echo json_encode($data_entry_day_circle, JSON_NUMERIC_CHECK);
-                            ?>
-              }]
-            });
-            console.log("datas loaded for zone lome");
-            var chartZoneLome = new CanvasJS.Chart("chartContainerZoneLome", {
-              animationEnabled: true,
-              exportEnabled: true,
-              title: {
-                text: "Rapport journalier global"
-              },
-              subtitles: [{
-                text: "Zone Lomé"
-              }],
-              data: [{
-                type: "pie",
-                showInLegend: "true",
-                legendText: "{label}",
-                indexLabelFontSize: 16,
-                indexLabel: "{label} - #percent%",
-                yValueFormatString: "฿#,##0",
-                dataPoints: <?php echo json_encode($data_entry_day_circle, JSON_NUMERIC_CHECK);
-                            ?>
-              }]
-            }); 
 
-
-           
-            chart1.render();
-
-            chartZoneLome.render();
-          }
-
-             // datas = document.getElementById("datas");
-         
-
-        </script>
       </div>
-      <div id="chartContainer" style="margin-left: 2vw; height: 30vh; position: absolute; right: 66vw; width: 35%;">
-        <div id="chartContainerZoneLome" style="margin-left: 2vw; height: 30vh; position: absolute; right: 60vw; width: 35%;">
+      <div id="chartContainer" style="margin-left: 2vw; height: 30vh; position: absolute; right: 66vw; width: 35%;"></div>
 
-        </div>
-        <div id="chartContainerBlock" style="font-family: 'Poppins', sans-serif; background-color: #f8fafb"></div>
 
-        <div class="circle"></div>
-        <div class="left">
-        </div>
+
+      <div id="chartContainerZoneLome" style="width: 20vw; height: 20vh;  position: fixed; left: 30vw">
       </div>
+      <!-- <div id="chartContainerBlock" style="font-family: 'Poppins', sans-serif; background-color: #f8fafb"></div> -->
 
-      <div class="ticket-open">
-        <div class="title">
-          <!-- Tickets ouverts -->
-        </div>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">ID Ticket</th>
-              <th scope="col">Type de demande</th>
-              <th scope="col">Equipement</th>
-              <th scope="col">Site</th>
-              <th scope="col">Adresse</th>
-              <th scope="col">Temps écoulé</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php while ($demande_info = $req->fetch()) {  ?>
-              <tr>
-                <td><?= $demande_info['id_ticket']; ?></td>
-                <td><?= $demande_info['type_demande']; ?></td>
-                <td><?= $demande_info['equipement']; ?></td>
-                <td><?= $demande_info['site_demande']; ?></td>
-                <td><?= $demande_info['add_by']; ?></td>
-                <td class="alert-time-left-init">
-                  <?php
-                  $date = new DateTime(htmlspecialchars($demande_info['date_envoi']));
-                  $jour = intval($date->diff($now)->format('%d'));
-                  $heure = intval($date->diff($now)->format('%h'));
-                  $minute = intval($date->diff($now)->format('%i'));
-                  if (($minute > 5) && ($jour > 1)) {
-                    echo "<div style='color: red'>" . $jour . " jour " . $heure . " heure " . $minute . " minute " . "</div>";
-                  } else {
-                    echo $minute . " minute ";
-                  }
-
-
-                  // echo $date->diff($now)->format('%d jour %h heure %i minutes');
-                  ?>
-                </td>
-              </tr>
-            <?php  } ?>
-          </tbody>
-        </table>
+      <div class="circle"></div>
+      <div class="left">
       </div>
     </div>
+
+    <div class="ticket-open">
+      <div class="title">
+        <!-- Tickets ouverts -->
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID Ticket</th>
+            <th scope="col">Type de demande</th>
+            <th scope="col">Equipement</th>
+            <th scope="col">Site</th>
+            <th scope="col">Adresse</th>
+            <th scope="col">Temps écoulé</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php while ($demande_info = $req->fetch()) {  ?>
+            <tr>
+              <td><?= $demande_info['id_ticket']; ?></td>
+              <td><?= $demande_info['type_demande']; ?></td>
+              <td><?= $demande_info['equipement']; ?></td>
+              <td><?= $demande_info['site_demande']; ?></td>
+              <td><?= $demande_info['add_by']; ?></td>
+              <td class="alert-time-left-init">
+                <?php
+                $date = new DateTime(htmlspecialchars($demande_info['date_envoi']));
+                $jour = intval($date->diff($now)->format('%d'));
+                $heure = intval($date->diff($now)->format('%h'));
+                $minute = intval($date->diff($now)->format('%i'));
+                if (($minute > 5) && ($jour > 1)) {
+                  echo "<div style='color: red'>" . $jour . " jour " . $heure . " heure " . $minute . " minute " . "</div>";
+                } else {
+                  echo $minute . " minute ";
+                }
+
+
+                // echo $date->diff($now)->format('%d jour %h heure %i minutes');
+                ?>
+              </td>
+            </tr>
+          <?php  } ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </body>
+<script defer>
+  document.addEventListener('DOMContentLoaded', (event) => {
+    // datas = document.getElementById("datas");
+    console.log("datas loaded");
+    var chart1 = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Rapport journalier global"
+      },
+      subtitles: [{
+        text: "Toutes zones incluses"
+      }],
+      data: [{
+        type: "pie",
+        showInLegend: "true",
+        legendText: "{label}",
+        indexLabelFontSize: 16,
+        indexLabel: "{label} - #percent%",
+        yValueFormatString: "฿#,##0",
+        dataPoints: <?php echo json_encode($data_entry_day_circle, JSON_NUMERIC_CHECK);
+                    ?>
+      }]
+    });
+    // console.log("datas loaded for zone lome");
+    console.log(document.querySelector('#chartContainerZoneLome'))
+    var chart2 = new CanvasJS.Chart("chartContainerZoneLome", {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Rapport journalier global"
+      },
+      subtitles: [{
+        text: "Zone Lomé"
+      }],
+      data: [{
+        type: "pie",
+        showInLegend: "true",
+        legendText: "{label}",
+        indexLabelFontSize: 16,
+        indexLabel: "{label} - #percent%",
+        yValueFormatString: "฿#,##0",
+        dataPoints: <?php echo json_encode($data_entry_day_circle, JSON_NUMERIC_CHECK);
+                    ?>
+      }]
+    });
+    // chart2.render()
+    chart1.render();
+    chart2.render()
+  })
+</script>
 
 </html>
